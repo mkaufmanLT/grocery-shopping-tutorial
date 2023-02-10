12d46ff5520e022c1e665c5109a6bfff9af75b66
mkaufman <mkaufman@leantechniques.com>
2023-02-10 10:48:15 -0600

    updates

--- app/recipes/recipe-list/recipe-list.component.ts
+++ app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe(
+      'Banana Peanut Butter Cookies',
+      '5 ingredients only!',
+      'https://kirbiecravings.com/wp-content/uploads/2021/08/peanut-butter-banana-cookies-6a.jpg')
+  ];
 
+  constructor() {
+  }
+
+  ngOnInit(){
+  }
 }
